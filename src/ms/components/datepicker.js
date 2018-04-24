e61be83891443719c0d66058dbf3ddb4acec33b3
windywany <windywany@163.com>
2018-04-24 20:28:35 +0800

    添加timepicker

--- src/ms/components/datepicker.js
+++ src/ms/components/datepicker.js
@@ -48,6 +48,32 @@
 			}
 		});
 	};
+	$.fn.wulauiTimepicker = function () {
+		return $(this).each((i, e) => {
+			const $this = $(e), inited = $this.data('TimepickerObj');
+			if (!inited) {
+				$this.data('TimepickerObj', 1);
+				let target = $this.attr('for');
+				if (target) {
+					target    = $(target);
+					let value = target.val(), values = value.split(':');
+					if (values.length === 0) {
+						values = ['00', '00'];
+					} else if (values.length === 1) {
+						values.push('00');
+					}
+					$this.find('.hour').on('change', function () {
+						values[0] = $(this).val();
+						target.val(values.join(':'));
+					}).val(values[0]);
+					$this.find('.minute').on('change', function () {
+						values[1] = $(this).val();
+						target.val(values.join(':'));
+					}).val(values[1]);
+				}
+			}
+		});
+	};
 	$('body').on('wulaui.widgets.init', '.wulaui', function (e) {
 		e.stopPropagation();
 		let that = $(this).find('[data-datepicker]');
@@ -59,6 +85,10 @@
 					that.wulauiDatepicker();
 				})
 			}
+		}
+		let tt = $(this).find('[data-timepicker]');
+		if (tt.length > 0) {
+			tt.wulauiTimepicker();
 		}
 	})
 })($);
