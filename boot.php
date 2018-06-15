8e7b60b182abdbdaf222e0099a19d3e206719081
windywany <windywany@163.com>
2018-06-15 17:33:02 +0800

    ready for 1.4.1

--- boot.php
+++ boot.php
@@ -7,7 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-define('JQ_VERSION', '1.4.0');
+define('JQ_VERSION', '1.4.1');
 
 /**
  * 加载界面样式资源.
