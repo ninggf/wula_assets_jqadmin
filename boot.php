a608fedccb3235eab653e3e93a55603c6e4dd264
windywany <windywany@163.com>
2018-09-04 16:34:56 +0800

    将layui升级到2.4.3

--- boot.php
+++ boot.php
@@ -7,7 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-define('JQ_VERSION', '2.1.0');
+define('JQ_VERSION', '2.4.3');
 
 /**
  * 加载界面样式资源.
